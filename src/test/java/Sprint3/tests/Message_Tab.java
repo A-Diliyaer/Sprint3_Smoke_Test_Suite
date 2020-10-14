d0ac627acc498a04914df3777e772517d66d22ba
cschmertz <cschmertz@gmail.com>
2020-10-14 17:39:30 -0400

    Adding project

--- src/test/java/Sprint3/tests/Message_Tab.java
+++ src/test/java/Sprint3/tests/Message_Tab.java
@@ -1,8 +1,10 @@
 package Sprint3.tests;
 
+import Sprint3.util.BrowserUtil;
 import Sprint3.util.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -37,6 +39,25 @@ public class Message_Tab {
         boolean topic_displayed = driver.findElement(By.xpath("//input[@id='POST_TITLE']")).isDisplayed();
 
         Assert.assertTrue(topic_displayed,"topic box is not displayed");
+    }
+
+    @Test
+    public void sprint3_story1_AC5() {
+        //Clicking Message Field
+        WebElement messageField = driver.findElement(By.xpath("//span[@class='feed-add-post-micro-title']"));
+        messageField.click();
+
+        //Clicking Quote Button
+        WebElement quoteButton = driver.findElement(By.xpath("//*[@id=\"bx-b-quote-blogPostForm\"]/span/i"));
+        quoteButton.click();
+        BrowserUtil.Wait(1);
+
+        //Confirming Created Quote Box
+        WebElement iframe = driver.findElement(By.xpath("//*[@id=\"bx-html-editor-iframe-cnt-idPostFormLHE_blogPostForm\"]/iframe"));
+        driver.switchTo().frame(iframe);
+        BrowserUtil.Wait(1);
+        WebElement quoteBox = driver.findElement(By.xpath("/html/body/blockquote"));
+        Assert.assertTrue(quoteBox.isDisplayed());
     }
 
     @AfterMethod
